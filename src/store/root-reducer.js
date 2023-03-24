faeb3998153abbe656d833165110a6ed16ff7895
Oleksii <o.rohochyi@astoundcommerce.com>
2023-03-24 23:56:19 +0200

    Implemented search

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { controlsReducer } from './controls/controls-reducer';
 import { countriesReducer } from './countries/countries-reducer';
 import { themeReducer } from './theme/theme-reducer';
 
 export const rootReducer = combineReducers({
     theme: themeReducer,
-    countries: countriesReducer
+    countries: countriesReducer,
+    controls: controlsReducer
 });
